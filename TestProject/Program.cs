7a11c7e97507aa4dcd04384624bf89f616e25957
Claudemir Junior <152872326+cx-claudemir-junior@users.noreply.github.com>
2025-06-26 12:28:45 +0100

    Add files via upload

---
+++ TestProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO.Abstractions;
+
+public class Program
+{
+    public static void Main(string[] args) {
+        var message = "Hello World";
+        var fileSystem = new FileSystem();
+        fileSystem.File.WriteAllTextAsync("test.log" , message);
+        Console.WriteLine(message);
+    }
+}
